c6e744fd7d70cf9812a7921bd4c7b6486314965f
Wilbur125 <matt.s.wilbur@gmail.com>
2020-12-12 18:13:58 -0800

    updating components and classes for app and home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
---
+++ src/components/pages/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import Navbar from '../navbar/Navbar';
+import About from '..about/About';
+
+
+class Home extends Component {
+    render() {
+        return (
+            <Header/>
+
+
+        )
+    }
+}
